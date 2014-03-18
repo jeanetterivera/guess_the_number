aaba45dc97fa68a3812b6443eea14ea94d04cb1a
jeanetterivera <jeanette_rivera1012@yahoo.com>
2014-03-18 18:05:54 +0000

    checked if it was correct

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,3 +1,12 @@
 puts "Welcome to guess the number"
-secret_number = rand(1012)
-puts secret_number
+secret_number = rand(10)
+
+puts "Guess a number between 0 and 9:"
+guess = gets.chomp.to_i 
+
+if secret_number == guess
+  puts "Congratualations ! You are correct"
+else
+  puts "Wrong ! #{secret_number} was the correct number"
+end
+

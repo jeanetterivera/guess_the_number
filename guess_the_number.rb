3ca944b969fa7325a68c66a8b75bbc9369297960
jeanetterivera <jeanette_rivera1012@yahoo.com>
2014-03-20 17:57:40 +0000

    Added spanish to the welcome to our game . Added ascii art to the code.

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,13 +1,19 @@
-puts "Welcome to guess the number"
-secret_number = rand(10)
+puts " bienvenidos to the impossible game of impossibleness  ! guess that numero "
+random_number = rand(11)
 
-puts "Guess a number between 0 and 9:"
+puts "Guess an impossible number between 0 and 10  .:"
 guess = gets.chomp.to_i 
 
-until guess == secret_number 
- puts "Wrong !try again"
+until guess == random_number
+puts "Wrong ! Its impossible try again ... to fail ;) "
     guess = gets.chomp.to_i
 end
  
+puts " Your correct ,  congratulations you defeated the impossible !"
 
+
+puts "      /\_/\
+          =( °w° )=
+            )   (  //
+           (__ __)// "
 
